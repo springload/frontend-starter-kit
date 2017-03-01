7ccfc144405b230845aa293fa70c1583f8eb421b
Vincent Audebert <vincent.audebert@yahoo.fr>
2017-03-02 09:08:01 +1300

    reintroducing dist and build. Watch through gulp with live reload and fix npm versions.

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,19 +1,29 @@
 var gulp = require("gulp");
-var config = require("../config");
 var path = require("path");
 var bs = require('browser-sync').create('main');
+var webpack = require('webpack');
+var config = require("../config");
+var webpackConfig = require('../../webpack/webpack.config.dev');
+var bundler = webpack(webpackConfig);
 
-gulp.task('watch', ['css'], function() {
+function bundleWatch(err) {
+    if (err) {
+        throw new Error(err);
+    }
+
+    if (bs.active) {
+        bs.reload(path.join(webpackConfig.output.path, '**', '*.js'));
+    }
+}
+
+gulp.task('watch', ['build'], function() {
     bs.init({
-        open: false,
-        //server: config.paths.views
-        proxy: 'example.com:1337'
+        proxy: 'localhost:3000',
+    }, function() {
+        bundler.watch(200, bundleWatch);
     });
 
-    var justReload = [
-        path.join(config.paths.views, '**', '*.html'),
-    ];
-
-    gulp.watch(justReload, bs.reload);
+    gulp.watch(path.join(config.paths.views, '**', '*.html'), bs.reload);
     gulp.watch(path.join(config.paths.sass, '**', '*.scss'), ['css']);
+    gulp.watch(path.join(config.paths.svg, '**', '*.svg'), ['svg']);
 });
