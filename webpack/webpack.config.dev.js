3b4ecc9a6d93742671afca712cae8eaf5face66b
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-13 23:39:25 +0200

    Fix webpack config in production (NODE_ENV)

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -4,21 +4,11 @@ const config = require('./webpack.config.js');
 config.devtool = 'cheap-module-source-map';
 config.watch = true;
 
-const prod = new webpack.DefinePlugin({
+config.plugins.push(new webpack.DefinePlugin({
     'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        NODE_ENV: JSON.stringify('development'),
     },
-});
-
-config.plugins.push(prod);
-
-// Some libraries import Node modules but don't use them in the browser.
-// Tell Webpack to provide empty mocks for them so importing them works.
-config.node = {
-    fs: 'empty',
-    net: 'empty',
-    tls: 'empty',
-};
+}));
 
 // Turn off performance hints during development because we don't do any
 // splitting or minification in interest of speed. These warnings become
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,10 +1,18 @@
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
 
+config.devtool = 'source-map';
 config.watch = false;
+
+config.plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+    },
+}));
+
 config.plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: {
-        screw_ie8: true, // React doesn't support IE8
+        screw_ie8: true,
         warnings: false,
     },
     mangle: {
