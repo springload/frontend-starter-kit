58d4d3b2d8fb2964d8973907b75b25c9c59f3c4a
Sam Sanders <samantha.k.sanders@gmail.com>
2018-04-13 15:03:43 +1200

    add watch command and mode relative to node env setting

--- django-wagtail/webpack.config.js
+++ django-wagtail/webpack.config.js
@@ -1,40 +1,43 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FixPaths = require('./webpack.fixpaths');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FixPaths = require("./webpack.fixpaths");
 
 module.exports = env => {
-  return {
-    entry: './core/static_src/index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'core/static'),
-    },
-    mode: 'development', // TODO: toggle this per env
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Output Management',
-        filename: path.resolve(__dirname, 'core/templates/base.html'),
-        template: path.resolve(__dirname, 'core/templates_src/base.html'),
-      }),
-      new FixPaths(),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.scss$/,
-          use: [
-            {
-              loader: 'style-loader', // creates style nodes from JS strings
-            },
-            {
-              loader: 'css-loader', // translates CSS into CommonJS
-            },
-            {
-              loader: 'sass-loader', // compiles Sass to CSS
-            },
-          ],
+    return {
+        entry: "./core/static_src/index.js",
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "core/static")
         },
-      ],
-    },
-  };
+        mode: process.env.NODE_ENV,
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Output Management",
+                filename: path.resolve(__dirname, "core/templates/base.html"),
+                template: path.resolve(
+                    __dirname,
+                    "core/templates_src/base.html"
+                )
+            }),
+            new FixPaths()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: "style-loader" // creates style nodes from JS strings
+                        },
+                        {
+                            loader: "css-loader" // translates CSS into CommonJS
+                        },
+                        {
+                            loader: "sass-loader" // compiles Sass to CSS
+                        }
+                    ]
+                }
+            ]
+        }
+    };
 };
