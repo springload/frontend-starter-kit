698b2ccbce18acb312103c71814449afcf11f2a7
Claire Campbell <cc@springlaod.co.nz>
2018-04-18 12:25:24 +1200

    updating weback syntax to fix linting issues

--- django-wagtail/webpack.fixpaths.js
+++ django-wagtail/webpack.fixpaths.js
@@ -1,17 +1,21 @@
-function FixPaths(options) {
-  // Webpack likes to make relative paths between its
-  // generated HTML and its bundles(etc), but the paths
-  // it generates aren't how Wagtail serves files so we
-  // need to rewrite the paths
+function FixPaths() {
+    // Webpack likes to make relative paths between its
+    // generated HTML and its bundles(etc), but the paths
+    // it generates aren't how Wagtail serves files so we
+    // need to rewrite the paths
 }
 
-FixPaths.prototype.apply = function(compiler) {
-  compiler.plugin('compilation', compilation => {
-    compilation.plugin('html-webpack-plugin-after-html-processing', data => {
-      // fix relative paths so that browsers can resolve them
-      data.html = data.html.replace(/\.\.\/static\//gi, '/static/');
+FixPaths.prototype.apply = function fixPathsApply(compiler) {
+    compiler.plugin('compilation', compilation => {
+        compilation.plugin(
+            'html-webpack-plugin-after-html-processing',
+            data => {
+                // fix relative paths so that browsers can resolve them
+                /* eslint-disable no-param-reassign */
+                data.html = data.html.replace(/\.\.\/static\//gi, '/static/');
+            },
+        );
     });
-  });
 };
 
 module.exports = FixPaths;
