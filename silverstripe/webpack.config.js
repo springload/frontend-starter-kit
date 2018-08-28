0fa5c326013efed5e24a6e99f572fd1b018f4857
Katie Day <katie@springload.co.nz>
2018-08-28 13:45:14 +1200

    add url-loader to fix paths from css files

--- silverstripe/webpack.config.js
+++ silverstripe/webpack.config.js
@@ -76,20 +76,24 @@ module.exports = (env, options) => ({
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: './fonts',
-                            publicPath: '../fonts',
-                        },
-                    },
+                    options.mode === 'development'
+                        ? { loader: 'url-loader' }
+                        : {
+                              loader: 'file-loader',
+                              options: {
+                                  name: '[name].[ext]',
+                                  outputPath: './fonts',
+                                  publicPath: '../fonts',
+                              },
+                          },
                 ],
             },
             {
                 test: /\.(gif|png|jpe?g)$/i,
                 use: [
-                    {
+                    options.mode === 'development'
+                        ? { loader: 'url-loader' }
+                        : {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
