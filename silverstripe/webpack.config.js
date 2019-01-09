5398eda46dec43bc7c107ef52a77c4eeb30e9597
Katie Day <katie@springload.co.nz>
2019-01-09 14:34:09 +1300

    Remove options and preset-flow from webpack config

--- silverstripe/webpack.config.js
+++ silverstripe/webpack.config.js
@@ -25,22 +25,6 @@ module.exports = (env, options) => ({
                 exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: {
-                                        browsers: [
-                                            'last 2 versions',
-                                            'safari >= 7',
-                                        ],
-                                    },
-                                },
-                            ],
-                            '@babel/preset-flow',
-                        ],
-                    },
                 },
             },
             {
